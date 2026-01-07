ee3b3d29c435af4ac0142f92581fbddc849a2df0
Jameson <bjameson9213@gmail.com>
2026-01-07 14:49:32 -0600

    Updating External Project Site URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/79745099_10217589843752055_2361073977947848704_n.jpg"
       />
-      <a href='https://imwa-project.netlify.app/' target='_blank'>Inventory Management Web Application</a>
+      <a href='https://imwa-project-app-2gyjc.ondigitalocean.app' target='_blank'>Inventory Management Web Application</a>
     </Layout>
 
   )
